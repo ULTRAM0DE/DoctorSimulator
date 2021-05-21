56749d4c83ac685e0fc56d55fce349de32f686df
0NE <0netw08778@gmail.com>
2021-05-21 21:27:47 +0400

    Добавьте файлы проекта.

---
+++ FilibusterSimulator/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DoctorSimulator.GameWindows;
+
+namespace DoctorSimulator
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			// Настройки окна
+			this.Height = 550;
+			this.Width = 1100;
+			this.BorderThickness = new Thickness(2, 10, 2, 2);
+
+			this.WindowStyle = WindowStyle.None;
+			this.ResizeMode = ResizeMode.NoResize;
+			this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+			// Инициализация изображения
+			imgMedicine.Source = WPF_Misc.ImageSourceFromBitmap(DoctorSimulator.Properties.Resources.ddd);
+		}
+
+		private void CloseWindow_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+		void BtnStartGame_Click(object sender, RoutedEventArgs e)
+		{
+			WPF_Misc.OpenNewWindow(this, new HomeWindow(this));
+			
+			this.Close();
+		}
+
+		void BtnLoadGame_Click(object sender, RoutedEventArgs e)
+		{
+			WPF_Misc.OpenPauseWindow(this, new LoadWindow(new HomeWindow(this), this));
+		}
+
+		void BtnExitGame_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
+		}
+
+
+	}
+}
---
+++ UnitTestProject2/Bisiness Logic/PersonTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DoctorSimulator.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DoctorSimulator.BusinessLogic.Tests
+{
+    [TestClass()]
+    public class PersonTests
+    {
+		Person person = new Person { Satiety = 100, Mood = 100, Money = 100 };
+
+		// Сытость
+		[TestMethod()]
+		public void SubstractSatiety_20()
+		{
+			Assert.IsTrue(person.SubstractSatiety(20));
+		}
+
+		[TestMethod()]
+		public void SubstractSatiety_120()
+		{
+			Assert.IsFalse(person.SubstractSatiety(120));
+		}
+
+		[TestMethod()]
+		public void SubstractSatiety_minus20()
+		{
+			Assert.IsFalse(person.SubstractSatiety(-20));
+		}
+
+
+		// Настроение
+		[TestMethod()]
+		public void SubstractMood_20()
+		{
+			Assert.IsTrue(person.SubstractMood(20));
+		}
+
+		[TestMethod()]
+		public void SubstractMood_120()
+		{
+			Assert.IsFalse(person.SubstractMood(120));
+		}
+
+		[TestMethod()]
+		public void SubstractMood_minus20()
+		{
+			Assert.IsFalse(person.SubstractMood(-20));
+		}
+
+		
+
+		// Золото
+		[TestMethod()]
+		public void SubstractGold_20()
+		{
+			Assert.IsTrue(person.SubstractGold(20));
+		}
+
+		[TestMethod()]
+		public void SubstractGold_120()
+		{
+			Assert.IsFalse(person.SubstractGold(120));
+		}
+
+		[TestMethod()]
+		public void SubstractGold_minus20()
+		{
+			Assert.IsFalse(person.SubstractGold(-20));
+		}
+	}
+}
